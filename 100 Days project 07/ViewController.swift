25da16da5ad49388d117c3d438368bba951040c6
EvgeniyKarachkov <cazinak341@gmail.com>
2021-05-22 11:39:04 +0300

    Finished project

--- 100 Days project 07/ViewController.swift
+++ 100 Days project 07/ViewController.swift
@@ -10,28 +10,45 @@ import UIKit
 
 class ViewController: UITableViewController {
     var petitions = [Petition]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        let urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+        }
         
         if let url = URL(string: urlString) {
-            if let data = try? Data(contentsOf: url)
-            parse(json: data)
+            if let data = try? Data(contentsOf: url){
+                parse(json: data)
+            } else {
+                showError()
+            }
+        } else {
+            showError()
         }
     }
-}
-
-func parce(json: Data) {
-    let decoder = JSONDecoder()
-
-    if let jsonPetitions = try? decoder.decode(Petitions.sels, from: json) {
-        petitions = jsonPetitions.results
-        tableView.reloadData()
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "Therewas a problem loadiding the feed; please check your connection and try again", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Ok", style: .default))
+        present(ac, animated: try)
     }
-}
-
+    
+    
+    func parce(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.sels, from: json) {
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+        }
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return petitions.count
     }
@@ -43,7 +60,13 @@ func parce(json: Data) {
         cell.detailTextLabel?.text = petition.body
         return cell
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, didSecetRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petition[indexPath.row]
+        UINavigationController?.pushViewController(vc, animated: true)
+    }
 }
+
+
 
